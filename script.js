61a355c99505121fbf5e524ba69cdd680b45f718
Mohamed <modernkille3grow@gmail.com>
2026-02-09 12:29:48 +0300

    added the basic functionality of MVP

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ letters.split("").forEach((letter) => {
 const key = document.getElementById("keyboard")
 key.setAttribute = "disabled"
 key.style.pointerEvents = "none"
+
+const input = document.getElementById("guess-input")
+const submit = document.getElementById("sBtn")
+let currentGuess = 1
+const maxGuesses = 6
+const count = document.getElementById("guess-count")
+function updateGuessDisplay() {
+  if (currentGuess < 6) {
+    currentGuess++
+    count.textContent = currentGuess + " / " + maxGuesses
+  } else if (currentGuess <= 6) {
+    alert("Game Over")
+    location.reload()
+  }
+}
+
+submit.addEventListener("click", () => {
+  const guess = input.value.toLowerCase()
+  if (guess === randomWord) {
+    alert("Congratulations! You've guessed the word!")
+  } else {
+    alert("Wrong guess! Try again.")
+    updateGuessDisplay()
+  }
+  console.log(guess)
+  input.value = ""
+})
+
+// make an array of words and then randomly select one for the user to guess
+const words = ["javascript", "hangman", "programming", "developer", "algorithm"]
+const randomWord = words[Math.floor(Math.random() * words.length)]
+console.log(randomWord) // for testing purposes
