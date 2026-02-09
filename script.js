f9deed5330d1efeefbd68433db067ad98e4ad735
Mohamed <modernkille3grow@gmail.com>
2026-02-09 09:25:05 +0300

    added the buttons and disabled them instead of making a div and a text for each letter

---
+++ script.js
@@ -0,0 +1,14 @@
+const keyboard = document.getElementById("keyboard")
+
+const letters = "abcdefghijklmnopqrstuvwxyz"
+
+letters.split("").forEach((letter) => {
+  const key = document.createElement("button")
+  key.textContent = letter.toUpperCase()
+  key.classList.add("key")
+  keyboard.appendChild(key)
+})
+// disable the button and instead of making a div or whatever having this is more convenient
+const key = document.getElementById("keyboard")
+key.setAttribute = "disabled"
+key.style.pointerEvents = "none"
